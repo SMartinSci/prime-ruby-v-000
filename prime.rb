9ac7d26eac9207dbc8840470f518651201bb3394
Sherry Martin <sm674@cornell.edu>
2018-09-17 13:28:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,19 @@
 # Add  code here!
 
-require 'prime'
-def prime?(integer)
-  integer.prime?
-end
+# require 'prime'
+# def prime?(integer)
+#   integer.prime?
+# end
+
+# #alternate way without prime?
+# def prime?(num)
+#   if num < 0 or num == 0 or num == 1
+#     return false
+#   else
+#     (2..num-1).to_a.all? do |possible_factor|
+#       num % possible_factor != 0
+#     end
+#   end
+# end
+
+# puts prime?(105557)
