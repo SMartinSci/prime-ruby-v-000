e1a2718e0a5a6504403333e8e7a6fc5d49be7e1b
Sherry Martin <sm674@cornell.edu>
2018-09-17 13:32:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -12,6 +12,8 @@ def prime?(num)
   true
 end
 
+# First, I check for 0 and 1, as they're not prime. Then check every number less than num to see if it divides. However, for every factor greater than the square root of num, there's one that's less, so we only look between 2 and the square root.
+
 # #alternate way without prime?
 # def prime?(num)
 #   if num < 0 or num == 0 or num == 1
